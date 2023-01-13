ae2436a7e86fabdd762c99807f7f448772435602
Manpreet-Singh001 <manpreetrahal27@gmail.com>
2023-01-13 14:39:27 -0700

    print self from url

--- lab-1.py
+++ lab-1.py
@@ -1,6 +1,10 @@
 import requests
 
-res = requests.get("https://www.google.com/")
-print(res.status_code)
-print(res.headers)
+#res = requests.get("https://www.google.com/")
+#print(res.status_code)
+#print(res.headers)
+#print(res.content)
+
+
+res = requests.get("https://raw.githubusercontent.com/Manpreet-Singh001/cmput-404-lab-1/main/lab-1.py")
 print(res.content)
